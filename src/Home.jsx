ba76c7122060e7f583ead0acf3d5a6236ca8fa85
Devapujyula Satya Sai Prakash <138996395+Devapujyula@users.noreply.github.com>
2026-03-25 17:26:55 +0530

    implemented login and logout functionalities

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,32 @@
+import Form from "./Form";
+
 export default function Home() {
+  var userId = localStorage.getItem("loggedInUser");
+
+  function logout() {
+    localStorage.clear();
+    window.location.reload();
+  }
+
   return (
     <>
-      <h2>Home Page</h2>
-      <a href="/create-account">SignUp</a>
-      <a href="/login">Login</a>
+      <div>
+        <h2>Home Page</h2>
+        {/* <a href="/create-account">SignUp</a>
+        <a href="/login">Login</a> */}
+        {userId == null && (
+          <div>
+            <a href="/create-account">SignUp</a>
+            <a href="/login">Login</a>
+          </div>
+        )}
+        {userId != null && (
+          <div>
+            <buton className="btn" onClick={(e) => logout(e)}></buton>
+          </div>
+        )}
+      </div>
+      {/* <Form /> */}
     </>
   );
 }
